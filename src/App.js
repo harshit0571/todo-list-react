847f92e376309d2e1803ab9780480f1337613d88
Harshit Chadha <harshitchadha@Harshits-MacBook-Pro.local>
2024-05-20 17:25:34 +0530

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import MainContainer from "./components/MainContainer";
-
-
 function App() {
- 
   return (
     <main>
       <Header />
